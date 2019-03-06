6b36ab27dc4655192222fbbf1728700514f3cdfb
yuwenjun32 <1398981815@qq.com>
2019-03-06 14:59:08 +0800

    完成首页面跳转VR图片和VR视频页面

--- app/src/main/java/com/example/stu/vr171032ywj/MainActivity.java
+++ app/src/main/java/com/example/stu/vr171032ywj/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.stu.vr171032ywj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView mIvMainPic;
+    private  ImageView mIvMainVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        mIvMainPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),VRPicActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mIvMainVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),VRPicActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        mIvMainPic=(ImageView)findViewById(R.id.iv_main_pic);
+        mIvMainVideo=(ImageView)findViewById(R.id.iv_main_video);
     }
 }
--- app/src/main/java/com/example/stu/vr171032ywj/VRPicActivity.java
+++ app/src/main/java/com/example/stu/vr171032ywj/VRPicActivity.java
@@ -3,11 +3,20 @@ package com.example.stu.vr171032ywj;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
 public class VRPicActivity extends AppCompatActivity {
 
+    private VrPanoramaView mVrMainPic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vrpic);
+        initView();
+    }
+
+    private void initView() {
+        mVrMainPic=(VrPanoramaView)findViewById(R.id.vr_main_pic);
+
     }
 }
