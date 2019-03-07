fc151026bffc943691633597a571c4e0e6cceb29
yuwenjun32 <1398981815@qq.com>
2019-03-07 12:00:19 +0800

    测试

--- app/src/main/java/com/example/stu/vr171032ywj/VRPicActivity.java
+++ app/src/main/java/com/example/stu/vr171032ywj/VRPicActivity.java
@@ -44,6 +44,7 @@ public class VRPicActivity extends AppCompatActivity {
                 InputStream is=getAssets().open("andes.jpg");
                 Bitmap bitmap=BitmapFactory.decodeStream(is);
                 is.close();
+                Toast.makeText(getApplicationContext(),"1111",Toast.LENGTH_LONG).show();
                 return bitmap;
             }catch (Exception e){
                 e.printStackTrace();
@@ -55,9 +56,11 @@ public class VRPicActivity extends AppCompatActivity {
         protected void onPostExecute(Bitmap bitmap) {//执行完主要的异步内容后调用
             super.onPostExecute(bitmap);
             if (bitmap!=null){
+                Toast.makeText(getApplicationContext(),"2222",Toast.LENGTH_LONG).show();
                 VrPanoramaView.Options options=new VrPanoramaView.Options();
                 options.inputType=VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
                 mVrMainPic.loadImageFromBitmap(bitmap,options);
+                Toast.makeText(getApplicationContext(),"3333",Toast.LENGTH_LONG).show();
             }
         }
     }
